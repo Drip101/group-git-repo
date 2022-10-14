73225fcb37fca0ad1c12c3b890cc28dbf673f456
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-10-13 20:57:51 -0400

    added 1st api

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let gyms = [
         distance: 0.3
 
     },
-// need to add images, addresses and descriptions to each gym location//
+    // need to add images, addresses and descriptions to each gym location//
     {
         title: "planet fitness",
         image: "pf.png",
@@ -28,6 +28,45 @@ let gyms = [
     }
 ]
 let ss = e => document.querySelector(e)
+
+let age = document.getElementById("age")
+let ageval
+console.log(age)
+
+let subscribe = document.getElementById("subscribe")
+console.log(subscribe)
+
+
+subscribe.addEventListener("click", () => {
+    console.log("hello")
+    ageval = age.value
+    //we have to add the url to the event listener locally to make sure it work.
+    let exampleUrl = 'https://fitness-calculator.p.rapidapi.com/bmi?age=' + ageval + '&weight=65&height=180'
+    console.log(age.value)
+    console.log(ageval)
+    console.log(exampleUrl)
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd06ec7497amsh95f885629733a6dp16d216jsn1b38d4de183c',
+            'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+        }
+    };
+    
+    fetch(exampleUrl, options)
+        .then(response => response.json())
+        .then(response => {
+            //console.log(response)
+        //console.log(response.data.bmi)
+        let bmi=response.data.bmi
+        //console.log(bmi)
+        })
+        .catch(err => console.error(err));
+})
+
+
+
+
 
 
 const showlocations = (g) => {
